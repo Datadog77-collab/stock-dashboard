2dea36819691e57ac1c8dd5a1788984f8cf99145
Datadog77-collab <oshimbe@gmail.com>
2026-03-07 20:12:52 +0900

    Update update_sheets.py

--- scripts/update_sheets.py
+++ scripts/update_sheets.py
@@ -4,6 +4,7 @@ from google.oauth2.service_account import Credentials
 
 scope = [
     "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
 ]
 
 creds = Credentials.from_service_account_file(
